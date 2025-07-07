76c0a193b271e6a3b1c61bec033304873c99fbe8
anchal12flyde <anchal@flyde.in>
2025-07-07 11:35:42 +0530

    Video Added

--- src/components/expro/ExproHeroSection.jsx
+++ src/components/expro/ExproHeroSection.jsx
@@ -22,7 +22,7 @@ export default function ExproHeroSection() {
             muted
             playsInline
           >
-            <source src="/videos/LightRays.mp4" type="video/mp4" />
+            <source src="/videos/expro.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
       
--- src/pages/Expro.jsx
+++ src/pages/Expro.jsx
@@ -4,6 +4,7 @@ import ExproHeroSection from "@/components/expro/ExproHeroSection"
 import D2C from "@/components/expro/D2C"
 import TechStack from "@/components/expro/TechStack"
 import Infrastructure from "@/components/expro/Infrastructure"
+import ExecutionCapability from "@/components/consult/ExecutionCapability"
 
 
 export default function Expro() {
@@ -11,11 +12,11 @@ export default function Expro() {
      return (
     <>
        <ExproHeroSection/>
+       <ExecutionCapability/>
        <D2C/>
        <TechStack/>
        <Infrastructure/>
-        <Footer/>
-           
+        <Footer/> 
            </>
             )
            }
