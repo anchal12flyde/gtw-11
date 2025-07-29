7a9190bd526fa45acab177a9cb6405684bc89672
Anchal <Anchal@flyde.in>
2025-07-29 14:57:39 +0530

    text change

--- src/components/expro/HeadlessSection.jsx
+++ src/components/expro/HeadlessSection.jsx
@@ -2,14 +2,11 @@
 
 export default function HeadlessSection() {
  return (
-    
-      <section className="hero  util-flex util-flex-1 util-mx-1-5 mt-26">
-        <h2 className="heading-systems">
-          What <span className="text-primary">You Get</span>
-        </h2>
-        <p className="subtext">
-          Shopify breaks when you scale. Headless is too headless. Piecing together tools creates operational chaos.
-        </p>
-      </section>
-      )
+   <section className="hero  util-flex util-flex-1 util-mx-1-5 mt-26">
+     <h2 className="heading-systems">
+       What <span className="text-primary">You Get</span>
+     </h2>
+     <p className="subtext">A modular, full-featured commerce backbone.</p>
+   </section>
+ );
     }
