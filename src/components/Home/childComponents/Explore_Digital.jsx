87fc1e600f9eb4d8361c048ddf8e40974723d2b3
Anchal <Anchal@flyde.in>
2025-07-30 11:29:35 +0530

    Live1

--- src/components/Home/childComponents/Explore_Digital.jsx
+++ src/components/Home/childComponents/Explore_Digital.jsx
@@ -23,13 +23,13 @@ export default function Explore_Digital() {
 
   return (
     <>
-      <section className="util-flex util-flex-1 util-mx-1-5 gradient-section text-white mt-26  usecases-section ">
+      <section className="util-flex util-flex-1 util-mx-1-5 gradient-section text-white-color1 mt-26  usecases-section ">
         <div className="lg:hidden absolute inset-0 flex items-center justify-center">
           <div className="flex flex-col items-center space-y-42 ">
-            <h2 className="text-[25vw] text-white/10 font-600 leading-none tracking-wider">
+            <h2 className="text-[25vw] text-white-color1/10 font-600 leading-none tracking-wider">
               From
             </h2>
-            <h2 className="text-[25vw] text-white/10 font-600 leading-none tracking-wider">
+            <h2 className="text-[25vw] text-white-color1/10 font-600 leading-none tracking-wider">
               To
             </h2>
           </div>
@@ -70,7 +70,7 @@ export default function Explore_Digital() {
               className="flex justify-center items-center"
             >
               <svg
-                className="w-50  text-primary animate-arrowDown lg:hidden"
+                className="w-50  text-light-blue animate-arrowDown lg:hidden"
                 viewBox="0 0 192 50"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -84,7 +84,7 @@ export default function Explore_Digital() {
                 />
               </svg>
               <svg
-                className="hidden lg:block w-8  text-primary animate-arrowDown"
+                className="hidden lg:block w-8  text-light-blue animate-arrowDown"
                 viewBox="0 0 24 104"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -107,7 +107,7 @@ export default function Explore_Digital() {
             >
               <p className="right-column-text justify-items-start">
                 We help organizations rethink the way they run, by{" "}
-                <span className="text-primary">
+                <span className="text-light-blue">
                   replacing complexity with clarity –
                 </span>{" "}
                 powered by purpose-built platforms.
@@ -146,108 +146,106 @@ export default function Explore_Digital() {
               </button>
             </div> */}
           </div>
-        </div>
 
-        <div
-          className="overflow-x-auto scroll-smooth snap-x snap-mandatory scrollbar-hide w-full flex"
-          ref={scrollRef}
-        >
-          <div className="flex flex-nowrap gap-10 ">
-            <div className=" yellow-container">
-              <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77  h-[510px] overflow-hidden">
-                <HoverVideo src="/videos/vd2.mp4" />
+          <div
+            className="overflow-x-auto scroll-smooth snap-x snap-mandatory scrollbar-hide w-full flex "
+            ref={scrollRef}
+          >
+            <div className="flex flex-nowrap gap-8 ">
+              <div className="">
+                <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77  h-[510px] overflow-hidden">
+                  <HoverVideo src="/videos/vd2.mp4" />
 
-                <div className="flex flex-col gap-4 px-8">
+                  <div className="flex flex-col gap-4 px-6">
+                    <h3 className="ecom-heading text-left">
+                      Numyum – Direct Ordering Engine for Cloud Kitchens
+                    </h3>
+                    <p className="paragraph-default">
+                      We built a custom food ordering system with OTP-based
+                      login, intuitive "Build Your Order" flow, and deep menu
+                      customizations. The platform supports multi-kitchen
+                      franchise operations, branch-specific pricing, and an
+                      integrated order approval system for seamless execution.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77 h-[510px] overflow-hidden ">
+                <div className="relative rounded-2xl ">
+                  <img
+                    src="/images/img4.png"
+                    alt="Campus"
+                    className="w-full h-55 "
+                  />
+                </div>
+                <div className="flex flex-col gap-4 px-6">
                   <h3 className="ecom-heading text-left">
-                    Numyum – Direct Ordering Engine for Cloud Kitchens
+                    EzStays – A Complete OS for Student Housing
+                  </h3>
+                  <p className="paragraph-default ">
+                    From admissions to mess management, EzStays is an end-to-end
+                    hostel management solution. We designed and developed a
+                    unified system covering room allocation, student onboarding,
+                    transport, payments, kitchen inventory, and more.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77  h-[510px] overflow-hidden">
+                <div className="relative rounded-2xl">
+                  <img
+                    src="/images/img7.png"
+                    alt="Campus"
+                    className="w-full h-55 "
+                  />
+                </div>
+                <div className="flex flex-col gap-4 px-6">
+                  <h3 className="ecom-heading text-left">
+                    Itel – Scalable Ecommerce for D2C Sales
                   </h3>
                   <p className="paragraph-default">
-                    We built a custom food ordering system with OTP-based login,
-                    intuitive "Build Your Order" flow, and deep menu
-                    customizations. The platform supports multi-kitchen
-                    franchise operations, branch-specific pricing, and an
-                    integrated order approval system for seamless execution.
+                    We developed a full-stack MERN-based ecommerce platform for
+                    Itel Mobiles, complete with live search recommendations,
+                    smart filtering, store locator, and Unicommerce integration
+                    — optimized for speed, reliability, and scale across India.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77  h-[510px] overflow-hidden">
+                <HoverVideo src="/videos/vd1.mp4" />
+
+                <div className="flex flex-col gap-4 px-6">
+                  <h3 className="ecom-heading text-left">
+                    TransExpert – A Digital Overhaul for a Logistics Group
+                  </h3>
+                  <p className="paragraph-default">
+                    For TransExpert’s three-brand group, we led a full corporate
+                    transformation — including brand identity design, corporate
+                    stationery, investor decks, and website UI/UX. We then
+                    developed all three brand websites to reflect a consistent,
+                    professional digital presence.
                   </p>
                 </div>
               </div>
             </div>
-
-            <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77 h-[510px] overflow-hidden ">
-              <div className="relative rounded-2xl ">
-                <img
-                  src="/images/img4.png"
-                  alt="Campus"
-                  className="w-full h-55 "
-                />
-              </div>
-              <div className="flex flex-col gap-4 px-8">
-                <h3 className="ecom-heading text-left">
-                  EzStays – A Complete OS for Student Housing
-                </h3>
-                <p className="paragraph-default ">
-                  From admissions to mess management, EzStays is an end-to-end
-                  hostel management solution. We designed and developed a
-                  unified system covering room allocation, student onboarding,
-                  transport, payments, kitchen inventory, and more.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77  h-[510px] overflow-hidden">
-              <div className="relative rounded-2xl">
-                <img
-                  src="/images/img7.png"
-                  alt="Campus"
-                  className="w-full h-55 "
-                />
-              </div>
-              <div className="flex flex-col gap-4 px-8">
-                <h3 className="ecom-heading text-left">
-                  Itel – Scalable Ecommerce for D2C Sales
-                </h3>
-                <p className="paragraph-default">
-                  We developed a full-stack MERN-based ecommerce platform for
-                  Itel Mobiles, complete with live search recommendations, smart
-                  filtering, store locator, and Unicommerce integration —
-                  optimized for speed, reliability, and scale across India.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-8 bg-white-color1 rounded-3xl w-70 lg:w-77  h-[510px] overflow-hidden">
-              <HoverVideo src="/videos/vd1.mp4" />
-
-              <div className="flex flex-col gap-4 px-8">
-                <h3 className="ecom-heading text-left">
-                  TransExpert – A Digital Overhaul for a Logistics Group
-                </h3>
-                <p className="paragraph-default">
-                  For TransExpert’s three-brand group, we led a full corporate
-                  transformation — including brand identity design, corporate
-                  stationery, investor decks, and website UI/UX. We then
-                  developed all three brand websites to reflect a consistent,
-                  professional digital presence.
-                </p>
-              </div>
-            </div>
-
-            <div className=" flex-shrink-0 w-0 "></div>
           </div>
-        </div>
 
-        <div className="flex justify-center  mt-6 sm:hidden">
-          <button className="arrow-button" onClick={scrollLeft}>
-            <img
-              src="/images/LeftArrow.png"
-              alt="Left Arrow"
-              className="w-10 h-10"
-            />
-          </button>
-          <button className="arrow-button" onClick={scrollRight}>
-            <img
-              src="/images/RightArrow.png"
-              alt="Right Arrow"
-              className="w-10 h-10"
-            />
-          </button>
+          <div className="flex justify-center  mt-6 sm:hidden">
+            <button className="arrow-button" onClick={scrollLeft}>
+              <img
+                src="/images/LeftArrow.png"
+                alt="Left Arrow"
+                className="w-10 h-10"
+              />
+            </button>
+            <button className="arrow-button" onClick={scrollRight}>
+              <img
+                src="/images/RightArrow.png"
+                alt="Right Arrow"
+                className="w-10 h-10"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </>
