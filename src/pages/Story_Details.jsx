64f1302cc36e9e8b73746cf2f0e149bb6b3d7ab3
Anchal <Anchal@flyde.in>
2025-07-31 03:08:36 +0530

    Live 5

--- src/pages/Story_Details.jsx
+++ src/pages/Story_Details.jsx
@@ -1,21 +1,21 @@
-'use client'
-import Header from "@/components/Home/childComponents/Header"
-import Footer from "@/components/Home/childComponents/Footer"
-import slug from "@/pages/Suc"
-import RebuildSection from "@/components/consult/RebuildSection"
-import Testimonial from "@/components/successstories/Testimonal"
-import ImpactVolume from "@/components/successstories/ImpactVolume"
+// 'use client'
+// import Header from "@/components/Home/childComponents/Header"
+// import Footer from "@/components/Home/childComponents/Footer"
+// import slug from "@/pages/Suc"
+// import RebuildSection from "@/components/consult/RebuildSection"
+// import Testimonial from "@/components/successstories/Testimonal"
+// import ImpactVolume from "@/components/successstories/ImpactVolume"
 
-export default function Story_Details() {
+// export default function Story_Details() {
 
-     return (
-    <>
-    <Header/>
-   <StoryDetails/>
-   <Testimonial/>
-   <ImpactVolume/>
-   <RebuildSection/>
-    <Footer/>
-    </>
-     )
-           }
+//      return (
+//     <>
+//     <Header/>
+//    <StoryDetails/>
+//    <Testimonial/>
+//    <ImpactVolume/>
+//    <RebuildSection/>
+//     <Footer/>
+//     </>
+//      )
+//            }
