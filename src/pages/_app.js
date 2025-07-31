b177ca177ba3e74a573111ab2cdeb0a6a5af0c4d
Anchal <Anchal@flyde.in>
2025-07-31 16:29:46 +0530

    Live 4:29

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,67 @@
-'use client'
-import { useState, useEffect } from 'react';
-import "@/app/globals.css"
-import Loader from '@/components/Home/Loader/Loader';
-import { FormProvider } from '@/context/FormContext';
-import { Toaster } from 'react-hot-toast';
+"use client";
+
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
+import Loader from "@/components/Home/Loader/Loader";
+import { FormProvider } from "@/context/FormContext";
+import { Toaster } from "react-hot-toast";
+import "@/app/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
+  const pathname = usePathname();
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1500); 
+    const timer = setTimeout(() => setLoading(false), 1500);
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
-      {loading ? <Loader /> :<FormProvider> <Component {...pageProps} /></FormProvider>}
-   <Toaster
-  position="top-right"
-  toastOptions={{
-    style: {
-      background: '#white', 
-      color: 'black', 
-      padding: '12px 20px',
-      borderRadius: '0.75rem',
-      boxShadow: '0 4px 10px rgba(0, 0, 0, 0.15)',
-      fontSize: '15px',
-    },
-  }}
-/>
+      {loading ? (
+        <Loader />
+      ) : (
+        <FormProvider>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={pathname}
+              initial={{ opacity: 0, y: -100 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.8,
+                  ease: [0.16, 0.77, 0.47, 0.97], // Smooth bounce effect
+                },
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  duration: 0.3,
+                  ease: "easeIn",
+                },
+              }}
+            >
+              <Component {...pageProps} />
+            </motion.div>
+          </AnimatePresence>
+        </FormProvider>
+      )}
 
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: "#ffffff",
+            color: "black",
+            padding: "12px 20px",
+            borderRadius: "0.75rem",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
+            fontSize: "15px",
+          },
+        }}
+      />
     </>
   );
 }
-
