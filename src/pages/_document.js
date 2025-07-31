440f24c9bdc50eaf72425426999cc5bcb4a85605
Anchal <Anchal@flyde.in>
2025-07-31 14:15:27 +0530

    Live 2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PBH6JYSWFK"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PBH6JYSWFK');
+            `,
+          }}
+        />
+
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           rel="stylesheet"
@@ -16,5 +36,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
