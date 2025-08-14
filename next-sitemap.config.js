76d4f49adecedaeeb9e2eb35f4d7dba1e2db7981
Anchal <Anchal@flyde.in>
2025-08-14 12:44:51 +0530

    robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,67 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://generaltechworks.com/", // Replace with your domain
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000, // (optional) split sitemaps if too large
+  siteUrl: "https://generaltechworks.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
   changefreq: "daily",
   priority: 0.7,
-  exclude: [""], // (optional) paths to exclude
+  exclude: [
+    "/private/",
+    "/temp/",
+    "/agency-partnership/",
+    "/animation/",
+    "/audit/",
+    "/join-our-team/",
+    "/thankyou-page/",
+    "/step-one-form/",
+    "/step-five/",
+    "/step-four/",
+    "/step-three-consult/",
+    "/step-three-infra/",
+    "/step-three-mobile/",
+    "/step-three-saas/",
+    "/step-three-web/",
+    "/step-two-form/",
+    "/style-guide/",
+  ],
   robotsTxtOptions: {
     policies: [
+      // Normal users
+      {
+        userAgent: "*",
+        disallow: [
+          "/private/",
+          "/temp/",
+          "/agency-partnership/",
+          "/animation/",
+          "/audit/",
+          "/join-our-team/",
+          "/thankyou-page/",
+          "/step-one-form/",
+          "/step-five/",
+          "/step-four/",
+          "/step-three-consult/",
+          "/step-three-infra/",
+          "/step-three-mobile/",
+          "/step-three-saas/",
+          "/step-three-web/",
+          "/step-two-form/",
+          "/style-guide/",
+        ],
+      },
       { userAgent: "*", allow: "/" },
+
+      // Googlebot
       { userAgent: "Googlebot", allow: "/" },
+
+      // Capital path block
+      { userAgent: "*", disallow: "/Audit/" },
+      { userAgent: "*", allow: "/audit/" },
     ],
+    additionalSitemaps: ["https://generaltechworks.com/sitemap.xml"],
+    additionalRobotsTxt: `
+# Host
+Host: https://generaltechworks.com/
+`,
   },
 };
