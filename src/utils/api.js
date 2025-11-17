797ac12dbfbb2f250212419306a3e0badf094edb
Sahil Kalkal <sahil@flyde.in>
2025-11-17 15:43:46 +0530

    backend integrated

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,10 +3,11 @@ import axios from "axios";
 // Simple axios instance
 const api = axios.create({
   baseURL: "https://gtw-backend-qhwv.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: false, 
+  withCredentials: false,
 });
 
 export default api;
